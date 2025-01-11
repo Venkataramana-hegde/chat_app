d9dbabae5167f76d6ddab08448f7a926f6d55f21
Venkataramana hegde <venkataramanahegde64@gmail.com>
2025-01-11 16:11:04 +0530

    Add homepage sidebar

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(cors({
 }))
 
 app.use("/api/auth", authRoutes)
-app.use("/api/message", messageRoutes)
+app.use("/api/messageS", messageRoutes)
 
 app.listen(PORT, () => {
     console.log("Server is running on port: " + PORT);
